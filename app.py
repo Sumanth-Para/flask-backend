e8be6fec11fadc089d8bfa7fea1836f38c1256c4
Sumanth-Para <125335968+Sumanth-Para@users.noreply.github.com>
2023-04-27 20:29:20 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+@app.route('/api/sum', methods=['POST'])
+def sum():
+    data = request.get_json()
+    num1 = data['num1']
+    num2 = data['num2']
+    result = num1 + num2
+    return jsonify({'result': result})
+
+if __name__ == '__main__':
+    app.run(debug=True)
